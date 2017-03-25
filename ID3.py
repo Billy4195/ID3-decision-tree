14c49af98e242a89a8a4aa9749fa57ce17d1e87b
Billy.Su <g4691821@gmail.com>
2017-03-25 22:12:51 +0800

    K-folding validation

--- ID3.py
+++ ID3.py
@@ -2,26 +2,35 @@ from tools import *
 from tree import Node
 import numpy as np
 
-data = read_csv_file('iris.data')
-data = np.array(data,dtype=[('sepal_len',float),('sepal_wid',float),('petal_len',float),('petal_wid',float),('class','S32')])
-node_count = 0
-node_count += 1
-root = Node(data,node_count)
-root.num = node_count
-print(calc_entrophy(data))
-cur_node = find_node_to_split(root)
-while cur_node != None:
-    (best_rem,best_thre,best_index) = find_mini_rem_feature(cur_node)
-    left,right = split(cur_node.data,best_index,best_thre)
-    cur_node.set_threshold(best_thre)
-    cur_node.set_threshold_index(best_index)
-    node_count += 1
-    cur_node.left = Node(left,node_count)
-    node_count += 1
-    cur_node.right = Node(right,node_count)
-    cur_node.leaf = False
+def ID3(K):
+    data = read_csv_file('iris.data')
+    data = np.array(data,dtype=[('sepal_len',float),('sepal_wid',float),('petal_len',float),('petal_wid',float),('class','S32')])
+    np.random.shuffle(data)
+    arraylist = np.split(data,K)
+    for idx in range(K):
+        training_data = arraylist[0]
+        testing_data = arraylist[idx]
+        for i in range(1,K):
+            if K != 1 and not i == idx:
+                training_data = np.append(training_data,arraylist[i])
+        node_count = 1
+        root = Node(training_data,node_count)
+        root.num = node_count
+        cur_node = find_node_to_split(root)
+        while cur_node != None:
+            (best_rem,best_thre,best_index) = find_mini_rem_feature(cur_node)
+            left,right = split(cur_node.data,best_index,best_thre)
+            cur_node.set_threshold(best_thre)
+            cur_node.set_threshold_index(best_index)
+            node_count += 1
+            cur_node.left = Node(left,node_count)
+            node_count += 1
+            cur_node.right = Node(right,node_count)
+            cur_node.leaf = False
+            cur_node = find_node_to_split(root)
+        draw_graph(root)
 
-    cur_node = find_node_to_split(root)
-draw_graph(root)
+        validate_tree(root,testing_data)
 
-validate_tree(root,data)
+ID3(5)
+
